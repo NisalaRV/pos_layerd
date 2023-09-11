861f6572d206a1ecbb7349ea237f3258fd3dfcd5
NisalaRV <nisalaravihara129@gmail.com>
2023-09-11 22:08:02 +0530

    update

---
+++ Back End/src/lk/ijse/pos/repositry/CustomerRepositry.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos.repositry;
+
+import lk.ijse.pos.entity.Customer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomerRepositry {
+
+    public ArrayList<Customer> viewAllCustomers(Connection connection) throws SQLException;
+    public boolean addCustomer(Connection connection, Customer customer) throws SQLException;
+    public boolean updateCustomr(Connection connection, Customer customer) throws SQLException;
+    public boolean deleteCustomer(Connection connection, String id) throws SQLException;
+}
